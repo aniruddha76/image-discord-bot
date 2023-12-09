70c920f793cb3990ef183d7f9700cf347ef9c63f
aniruddha <aniruddhathakur76@gmail.com>
2023-12-09 21:12:59 +0530

    Image Discord bot

---
+++ images.js
@@ -0,0 +1,30 @@
+import { parse } from "node-html-parser";
+
+let imageUrl = [];
+
+    async function getHtml(url) {
+        let response = await fetch(url);
+        if (response.status == 200) {
+            var responseText = await response.text();
+            return responseText;
+        } else {
+            return "<html><body>page not found</body></html>";
+        }
+
+    }
+
+    async function run() {
+        let html = await getHtml("https://pinterest.com/albert_12_12/teagan-croft/");
+
+        let document = parse(html);
+        var urlLoop = document.querySelectorAll("img");
+
+        // console.log(loop);
+        for (let i = 0; i < urlLoop.length; i++) {
+            let urls = urlLoop[i].getAttribute("src");
+            imageUrl.push(urls)
+        }
+
+        return imageUrl;
+};
+export default run;
---
+++ index.js
@@ -0,0 +1,44 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import dotenv from "dotenv";
+dotenv.config();
+
+import run from "./images.js";
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ] 
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith('feed')){
+        try {
+            const imageUrl = await run();
+            imageUrl.map((item) => {
+                message.reply({
+                    content: `Generating Short ID for ${item}`,
+                });
+            })
+        } catch (error) {
+            console.error('Error fetching image:', error);
+            message.reply({
+                content: 'An error occurred while fetching the image.',
+            });
+        }
+    } else {
+        message.reply({
+            content: 'Hi From bot',
+        });
+    }
+});
+
+client.on('interactionCreate', interaction => {
+    // console.log(interaction)
+    interaction.reply("Pong!!")
+});
+
+client.login(process.env.BOT_TOKEN)
