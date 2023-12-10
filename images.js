6fab062f4bc173d1a5247f555a907b8e6c9d16a4
aniruddha <aniruddhathakur76@gmail.com>
2023-12-10 17:43:28 +0530

    Fixed Image Quality Issue

--- images.js
+++ images.js
@@ -2,29 +2,40 @@ import { parse } from "node-html-parser";
 
 let imageUrl = new Set();
 
-    async function getHtml(url) {
-        let response = await fetch(url);
-        if (response.status == 200) {
-            var responseText = await response.text();
-            return responseText;
-        } else {
-            return "<html><body>page not found</body></html>";
-        }
-
+async function getHtml(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        var responseText = await response.text();
+        return responseText;
+    } else {
+        return "<html><body>page not found</body></html>";
     }
 
-    async function run() {
-        let html = await getHtml("https://pinterest.com/albert_12_12/teagan-croft/");
+}
 
-        let document = parse(html);
-        var urlLoop = document.querySelectorAll("img");
+async function run() {
+    let html = await getHtml("https://pinterest.com/albert_12_12/teagan-croft/");
 
-        // console.log(loop);
-        for (let i = 0; i < urlLoop.length; i++) {
-            let urls = urlLoop[i].getAttribute("src");
-            imageUrl.add(urls)
+    let document = parse(html);
+    var images = document.querySelectorAll("img");
+
+    await Promise.all(images.map(async (image) => {
+        let urls = image.getAttribute('src');
+
+        if (urls.split("/")[3].includes("x")) {
+            let modifiedUrl = urls.replace(urls.split("/")[3], "originals");
+            let response = fetch(modifiedUrl);
+            if ((await response).status === 200) {
+                imageUrl.add(modifiedUrl)
+            } else {
+                let modifiedUrl2 = modifiedUrl.replace(modifiedUrl.split(".")[3], "webp");
+                imageUrl.add(modifiedUrl2);
+            }
         }
+    }));
 
-        return imageUrl;
-};
+    // console.log(imageUrl);
+    return imageUrl;
+}
+run()
 export default run;
--- index.js
+++ index.js
@@ -4,25 +4,25 @@ dotenv.config();
 
 import run from "./images.js";
 
-const client = new Client({ 
+const client = new Client({
     intents: [
-        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent
-    ] 
+    ]
 });
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
 
-    if (message.content.startsWith('feed')){
+    if (message.content.startsWith('feed')) {
         try {
-            const imageUrl = await run();
-            imageUrl.forEach((item) => {
-                message.reply({
-                    content: item
-                });
-            })
+                const imagesToSend = await run();
+                imagesToSend.forEach(async (item) => {
+                    message.reply({
+                        content: item
+                    })
+                })
         } catch (error) {
             console.error('Error fetching image:', error);
             message.reply({
@@ -30,10 +30,10 @@ client.on('messageCreate', async (message) => {
             });
         }
     } else {
-        message.reply({
-            content: 'Hi From bot',
-        });
-    }
+    message.reply({
+        content: 'Hi From bot',
+    });
+}
 });
 
 client.on('interactionCreate', interaction => {
