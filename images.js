9e9b735ca780dc342d2e427659e35d8a3eb1dd56
aniruddha <aniruddhathakur76@gmail.com>
2023-12-13 23:36:48 +0530

    Added random url send feature

--- images.js
+++ images.js
@@ -11,8 +11,17 @@ async function getHtml(url) {
 
 }
 
+async function shuffleArray (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+}
+
 async function run(nameToSearch) {
-    let userWant = nameToSearch.toLowerCase(); 
+    let userWant = nameToSearch.toLowerCase();
 
     let celebFirstName = userWant.split(" ")[0];
     let celebLastName = userWant.split(" ")[1];
@@ -26,42 +35,35 @@ async function run(nameToSearch) {
     var images = document.querySelectorAll("a img");
 
     await Promise.all(images.map(async (image) => {
-        try{
-        let urls = image.parentNode.rawAttributes.href;
+        try {
+            let urls = image.parentNode.rawAttributes.href;
 
-        if(urls.includes(celebFirstName)){
-            if(urls.startsWith("https:") && !urls.endsWith(".html")){
+            if (urls.includes(celebFirstName)) {
+                if (urls.startsWith("https:") && !urls.endsWith(".html")) {
 
-                imageUrl.add(urls)
+                    imageUrl.add(urls)
 
-            } else if(!urls.startsWith("https:") && !urls.endsWith(".html")) {
+                } else if (!urls.startsWith("https:") && !urls.endsWith(".html")) {
 
-                let newUrl = "https:" + urls;
-                imageUrl.add(newUrl);
+                    let newUrl = "https:" + urls;
+                    imageUrl.add(newUrl);
 
-            } else if(!urls.startsWith("https:") && urls.endsWith(".html")){
-                let newUrl = "https://www.aznude.com" + urls;
-                let response = await getHtml(newUrl);
-                let document = parse(response);
-                let video = document.querySelectorAll('.videoButtons').filter(element => element._attrs.class === 'videoButtons');
-                let href = video.filter(element => element.rawAttrs)[2].parentNode.rawAttrs;
-                let newVidUrl = "https:" + href.split('"')[1];
-                imageUrl.add(newVidUrl)
+                } else if (!urls.startsWith("https:") && urls.endsWith(".html")) {
+                    let newUrl = "" + urls;
+                    let response = await getHtml(newUrl);
+                    let document = parse(response);
+                    let video = document.querySelectorAll('.videoButtons').filter(element => element._attrs.class === 'videoButtons');
+                    let href = video.filter(element => element.rawAttrs)[2].parentNode.rawAttrs;
+                    let newVidUrl = "https:" + href.split('"')[1];
+                    imageUrl.add(newVidUrl)
+                }
             }
-        }
-    } catch (error) {}
-
-        // if (urls.split("/")[3].includes("x")) {
-        //     let modifiedUrl = urls.replace(urls.split("/")[3], "originals");
-        //     let response = fetch(modifiedUrl);
-        //     if ((await response).status === 200) {
-        //         imageUrl.add(modifiedUrl)
-        //     } else {
-        //         let modifiedUrl2 = modifiedUrl.replace(modifiedUrl.split(".")[3], "webp");
-        //         imageUrl.add(modifiedUrl2);
-        //     }
-        // }
+        } catch (error) { }
     }));
+
+    let urlArray = Array.from(imageUrl);
+    shuffleArray(urlArray)
+    imageUrl = new Set(urlArray)
 
     return imageUrl;
 }
